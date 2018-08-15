e89a88946adde90c0cc94343ae55e720a4ce0494
onursamilsahin <samilsahin4118@gmail.com>
2018-08-15 12:36:03 +0300

    Add project files.

---
+++ MetotUygulama/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotUygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //console üzerinde Hesap makinesi yapımı
+            while (true)
+            {
+
+                Console.WriteLine("Seçim Yapınız.");
+                Console.WriteLine("1-Toplama");
+                Console.WriteLine("2-çıkarma");
+                Console.WriteLine("3-çarpma");
+                Console.WriteLine("4-bölme");
+                Console.WriteLine("5-çıkış");
+                int secim = int.Parse(Console.ReadLine());
+
+                switch (secim)
+                {
+
+                    case 1:
+                        Console.WriteLine("toplanıcak değerleri giriniz(5+5+5):");
+                        string topla = Console.ReadLine();
+                        ToplamSonuc(topla);
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Çıkarılacak değerleri giriniz(5-5-5):");
+                        string cikar = Console.ReadLine();
+                        CikarmaSonuc(cikar);
+                        Console.ReadLine();
+                        //************************
+                        break;
+                    case 3:
+                        Console.WriteLine("******************Çarpma yapılacak değerleri giriniz(5*5*5):");
+                        string carpma = Console.ReadLine();
+                        Carpma(carpma);
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.WriteLine("Bölünecek değerleri giriniz(5/5/5):");
+                        string Bol = Console.ReadLine();
+                        Bolme(Bol);
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+
+                        break;
+                    default:
+                        Console.WriteLine("Yanlış tuşlama yaptınız çıkış yapmak istiyormusunuz?(Evet:E,Hayır:H)");
+                        string cevap = Console.ReadLine();
+                        if (cevap == "E")
+                        {
+                            Environment.Exit(0);
+                        }
+
+                        break;
+
+
+                }
+            }
+        }
+
+        private static  void ToplamSonuc(string deger)
+        {
+            int AraDeger = 0;
+
+
+            string[] sayilar = deger.Split('+');
+
+
+            foreach (var item in sayilar)
+            {
+                
+                AraDeger = AraDeger + Topla(0, item);
+            }
+
+            Console.WriteLine("Sonuç:{0}", AraDeger);
+        }
+        private static int Topla(int sayi1,string sayi2)
+        {
+            int deger = sayi1 + int.Parse(sayi2);
+
+
+            return deger;
+
+        }
+
+        private static void CikarmaSonuc(string deger)    
+        {
+            int sayac = 0;
+            int AraDeger = 0;
+
+            string[] sayilar = deger.Split('-');
+
+            foreach (var item in sayilar)
+            {
+
+                AraDeger =AraDeger  -int.Parse(item);
+                if (sayac == 0)
+                {
+                    AraDeger = AraDeger * (-1);
+                }
+
+                sayac++;
+            }
+
+
+
+            Console.WriteLine("Sonuç:{0}",AraDeger);
+
+
+        }
+
+        private static void Bolme(string deger)
+        {
+            string[] sayilar = deger.Split('/');
+
+            decimal Aradeger = 1;
+            int sayac = 0;
+            foreach (var item in sayilar)
+            {
+                Aradeger = Aradeger/ decimal.Parse(item);
+                if (sayac==0)
+                {
+                    Aradeger = decimal.Parse(item);
+                }
+
+                sayac++;
+            }
+
+
+
+            Console.WriteLine("Sonuç:{0}", Aradeger);
+
+
+        }
+
+
+        private static void Carpma(string Deger)
+        {
+            string[] Sayilar = Deger.Split('*');
+
+            int AraDeger = 1;
+
+            foreach (var item in Sayilar)
+            {
+
+                AraDeger = AraDeger * int.Parse(item);
+
+            }
+
+
+            Console.WriteLine("Sonuç:{0}",AraDeger);
+
+        }
+
+       
+
+
+
+
+    }
+}
---
+++ csharp3.0_Metot/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp3._0_Metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            IlkMetot();
+            EkranaYAz("Merhaba dünya");
+
+            Console.WriteLine(Topla(1, 2));
+            Console.WriteLine(Topla("onur","şahin"));
+            Console.ReadLine();
+
+
+        }
+        //Metotlar classlar içerisine yazılır .
+        //private :class içerisinde erişilebilir.
+        //Public:Class ın dışında örneklendiğinde veya  static is çağırıldığında gelir.
+
+
+        private static  void IlkMetot()
+        {
+
+            Console.WriteLine("Mehaba dünya");
+        }
+
+
+        static void EkranaYAz(string Metin)
+        {
+            Console.WriteLine(Metin);
+
+
+        }
+
+        //Aşırı yükleme aynı isimli birden fazla metot, Topla metotu aşırı yüklenmiştir.
+        static int Topla(int sayi1,int sayi2)
+        {
+
+            int sonuc = sayi1 + sayi2;
+
+            return sonuc;
+
+        }
+        //Aynı isimli iki adet metot yazılmıştır ancak parametleri ve geri dönüş tipleri farklı olduğu için aynı isimli olması bir problem yaratmaz.
+        static string Topla(string metin1, string metin2)
+        {
+
+            string sonuc = metin1 +" şamil "+ metin2;
+
+            return sonuc;
+
+        }
+
+
+
+        //Aşırı yükleme
+
+
+
+    }
+    //burayada class yazabiliriz. 
+}
